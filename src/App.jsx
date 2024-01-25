9227a7167d5ab8173877833873785fb9c7f35ec1
MarcosOkamoto1 <157605474+MarcosOkamoto1@users.noreply.github.com>
2024-01-25 14:05:04 +0000

    Criação Elementos CSS

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import './App.css'
 function App() {
   return (
     <>
-      <div>
+      <div className='card'>
         <h2>Rick Sanchez</h2>
-        <img className='Card' src='https://rickandmortyapi.com/api/character/avatar/1.jpeg'></img>
+        <img src='https://rickandmortyapi.com/api/character/avatar/1.jpeg'></img>
       </div>
     </>
   )
