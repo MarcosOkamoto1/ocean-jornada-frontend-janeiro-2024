0354530b2f99db95c9b42ae26f2b5ffa27006bf6
MarcosOkamoto1 <157605474+MarcosOkamoto1@users.noreply.github.com>
2024-01-25 14:41:49 +0000

    Estilização Completa do Card

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
     <>
       <div className='card'>
         <h2>Rick Sanchez</h2>
-        <img src='https://rickandmortyapi.com/api/character/avatar/1.jpeg'></img>
+        <img className='containerimage' src='https://rickandmortyapi.com/api/character/avatar/1.jpeg'></img>
       </div>
     </>
   )
